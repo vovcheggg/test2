8b5e7897860cdccd1239c7faf9e89833a5bb6398
galanov <8sergei1>
2015-06-22 23:20:10 +0600

    123

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -11,6 +11,7 @@ public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello world!");
+		//add first
 		return "hello";
 	}
 }
